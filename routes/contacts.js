f8f8f13f8d96c6b3b7771abf520c18735a98ac33
Black <gmocore@gmail.com>
2019-06-13 14:37:44 -0600

    express server setup with initial routes

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+//@route    GET api/contacts
+//@desc     get all user's contacts
+//@access   private
+router.get("/", (req, res) => {
+  res.send("get all constacts");
+});
+
+//@route    POST api/contacts
+//@desc     add new contacts
+//@access   private
+router.post("/", (req, res) => {
+  res.send("Add contact");
+});
+
+//@route    PUT api/contacts/:id
+//@desc     update contact
+//@access   private
+router.put("/:id", (req, res) => {
+  res.send("Update contact");
+});
+
+//@route    DELETE api/contacts/:id
+//@desc     delete conatct
+//@access   private
+router.delete("/:id", (req, res) => {
+  res.send("Delete conatct");
+});
+
+module.exports = router;

1cff3e671788d1edfd55ac4e1bee209fba6f2707
Black <gmocore@gmail.com>
2019-06-18 12:23:43 -0600

    added CSS transitions to delete, and filter. filter functionality

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
 import Contactform from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
 
 const Home = () => {
-  return (
-    <div className="grid-2">
-      <div>
-        <Contactform />
-      </div>
-      <div>
-        <Contacts />
-      </div>
-    </div>
-  );
+    return (
+        <div className="grid-2">
+            <div>
+                <Contactform />
+            </div>
+            <div>
+                <ContactFilter />
+                <Contacts />
+            </div>
+        </div>
+    );
 };
 
 export default Home;

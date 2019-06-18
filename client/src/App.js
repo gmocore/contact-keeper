a59f9b1f0e2e2f7935c89a0185093bd54d411be3
Black <gmocore@gmail.com>
2019-06-18 11:40:38 -0600

    added update contact functionality to form

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,21 @@ import ContactState from "./context/contact/ContactState";
 import "./App.css";
 
 const App = () => {
-  return (
-    <ContactState>
-      <Router>
-        <Fragment className="App">
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
-  );
+    return (
+        <ContactState>
+            <Router>
+                <Fragment className="App">
+                    <Navbar />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/about" component={About} />
+                        </Switch>
+                    </div>
+                </Fragment>
+            </Router>
+        </ContactState>
+    );
 };
 
 export default App;

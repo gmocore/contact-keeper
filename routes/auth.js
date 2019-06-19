634e51cf7c60fb403e93d793211552a291d19a33
Black <gmocore@gmail.com>
2019-06-19 14:11:00 -0600

    added alerts

--- routes/auth.js
+++ routes/auth.js
@@ -12,66 +12,66 @@ const User = require("../models/User");
 //@desc     get logged in user
 //@access   private
 router.get("/", auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select("-password");
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("server error");
-  }
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
 });
 
 //@route    POST api/auth
 //@desc     Auth user and get token
 //@access   public
 router.post(
-  "/",
-  [
-    check("email", "Please include a valid email").isEmail(),
-    check("password", "password is required").exists()
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-      if (!user) {
-        return res.status(400).json({ msg: "Invalid credentials" });
-      }
-
-      const isMatch = await bcrypt.compare(password, user.password);
-
-      if (!isMatch) {
-        return res.status(400).json({ msg: "Invalid credentials" });
-      }
-
-      const payload = {
-        user: {
-          id: user.id
+    "/",
+    [
+        check("email", "Please include a valid email").isEmail(),
+        check("password", "password is required").exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-      };
 
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        {
-          expiresIn: 360000
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ msg: "Invalid credentials" });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ msg: "Invalid credentials" });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
         }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server error");
     }
-  }
 );
 
 module.exports = router;
